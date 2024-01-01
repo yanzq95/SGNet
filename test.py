6df7d9c0c4d78ac8b248337e5ea68baac89e601d
Zhiqiang Yan <74827816+yanzq95@users.noreply.github.com>
2024-01-01 20:36:24 +0800

    Update test.py

--- test.py
+++ test.py
@@ -24,29 +24,111 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-net = SGNet(num_feats=40, kernel_size=3, scale=8)
-net.load_state_dict(torch.load("/opt/data/private/SGNet/SGNet_X8.pth", map_location='cuda:0'))
+parser = argparse.ArgumentParser()
+parser.add_argument('--scale', type=int, default=16, help='scale factor')
+parser.add_argument("--num_feats", type=int, default=40, help="channel number of the middle hidden layer")
+parser.add_argument("--root_dir", type=str, default='/datapath/RGB-D-D', help="root dir of dataset")
+parser.add_argument("--model_dir", type=str, default="/SGNet/ckpt/SGNet_X8_R.pth", help="path of model")
+parser.add_argument("--results_dir", type=str, default='SGNet/results', help="root dir of results")
+
+opt = parser.parse_args()
+
+net = SGNet(num_feats=opt.num_feats, kernel_size=3, scale=opt.scale)
+net.load_state_dict(torch.load(opt.model_dir, map_location='cuda:0'))
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 net.to(device)
 
 data_transform = transforms.Compose([transforms.ToTensor()])
-dataset = NYU_v2_datset(root_dir='/opt/data/share/120106010699/nyu_data', scale=8, transform=data_transform, train=False)
+
+dataset_name = opt.root_dir.split('/')[-1]
+if dataset_name == 'nyu_data':
+    dataset = NYU_v2_datset(root_dir=opt.root_dir, scale=opt.scale, transform=data_transform, train=False)
+    test_minmax = np.load('%s/test_minmax.npy' % opt.root_dir)
+    rmse = np.zeros(449)
+elif dataset_name == 'RGB-D-D':
+    dataset = RGBDD_Dataset(root_dir=opt.root_dir, scale=opt.scale, downsample='sync', train=False, transform=data_transform)
+    rmse = np.zeros(405)
+elif dataset_name == 'Middlebury':
+    dataset = Middlebury_dataset(root_dir=opt.root_dir, scale=opt.scale, transform=data_transform)
+    rmse = np.zeros(30)
+elif dataset_name == 'Lu':
+    dataset = Middlebury_dataset(root_dir=opt.root_dir, scale=opt.scale, transform=data_transform)
+    rmse = np.zeros(6)
+
 dataloader = DataLoader(dataset, batch_size=1, shuffle=False, num_workers=8)
 data_num = len(dataloader)
 
-rmse = np.zeros(449)
-mad = 0.0
-test_minmax = np.load('/opt/data/share/120106010699/nyu_data/test_minmax.npy')
+
 with torch.no_grad():
     net.eval()
-    for idx, data in enumerate(dataloader):
-        guidance, lr, gt = data['guidance'].cuda(), data['lr'].cuda(), data['gt'].cuda()
-        out, out_grad = net((guidance, lr))
-        minmax = test_minmax[:, idx]
-        minmax = torch.from_numpy(minmax).cuda()
-        rmse[idx] = calc_rmse(gt[0, 0], out[0, 0], minmax)
-        print(rmse[idx])
-    print(rmse.mean())
+    if dataset_name == 'nyu_data':
+        for idx, data in enumerate(dataloader):
+            guidance, lr, gt = data['guidance'].to(device), data['lr'].to(device), data['gt'].to(device)
+            out, out_grad = net((guidance, lr))
+            minmax = test_minmax[:, idx]
+            minmax = torch.from_numpy(minmax).cuda()
+            rmse[idx] = calc_rmse(gt[0, 0], out[0, 0], minmax)
+            
+            path_output = '{}/output'.format(opt.results_dir)
+            os.makedirs(path_output, exist_ok=True)
+            path_save_pred = '{}/{:010d}.png'.format(path_output, idx)
+            
+            # Save results  (Save the output depth map)
+            pred = out[0,0] * (minmax[0] - minmax[1]) + minmax[1]
+            pred = pred * 1000.0
+            pred = pred.cpu().detach().numpy()
+            pred = pred.astype(np.uint16)
+            pred = Image.fromarray(pred)
+            pred.save(path_save_pred)
+            
+            # visualization  (Visual depth map)
+            #pred = out[0, 0]
+            #pred = pred.cpu().detach().numpy()
+            #cv2.imwrite(path_save_pred, pred * 255.0)   
+            
+            print(rmse[idx])
+        print(rmse.mean())
+    elif dataset_name == 'RGB-D-D':
+        for idx, data in enumerate(dataloader):
+            guidance, lr, gt, maxx, minn = data['guidance'].to(device), data['lr'].to(device), data['gt'].to(device), data[
+                'max'].to(device), data['min'].to(device)
+            out, out_grad = net((guidance, lr))
+            minmax = [maxx, minn]
+            rmse[idx] = rgbdd_calc_rmse(gt[0, 0], out[0, 0], minmax)
+            
+            path_output = '{}/output'.format(opt.results_dir)
+            os.makedirs(path_output, exist_ok=True)
+            path_save_pred = '{}/{:010d}.png'.format(path_output, idx)
+            
+            # Save results  (Save the output depth map)
+            pred = out[0, 0] * (maxx - minn) + minn
+            pred = pred.cpu().detach().numpy()
+            pred = pred.astype(np.uint16)
+            pred = Image.fromarray(pred)
+            pred.save(path_save_pred)
+            
+            # visualization  (Visual depth map)
+            #pred = out[0, 0]
+            #pred = pred.cpu().detach().numpy()
+            #cv2.imwrite(path_save_pred, pred * 255.0)   
+            print(rmse[idx])
+        print(rmse.mean())
+    elif (dataset_name == 'Middlebury') or (dataset_name == 'Lu'):
+        for idx, data in enumerate(dataloader):
+            guidance, lr, gt = data['guidance'].to(device), data['lr'].to(device), data['gt'].to(device)
+            out, out_grad = net((guidance, lr))
+            rmse[idx] = midd_calc_rmse(gt[0, 0], out[0, 0])
+            
+            path_output = '{}/output'.format(opt.results_dir)
+            os.makedirs(path_output, exist_ok=True)
+            path_save_pred = '{}/{:010d}.png'.format(path_output, idx)
+            
+            # Save results  (Save the output depth map)
+            pred = out[0,0] * 255.0
+            pred = pred.cpu().detach().numpy()
+            pred = pred.astype(np.uint16)
+            pred = Image.fromarray(pred)
+            pred.save(path_save_pred)
 
-
-
+            print(rmse[idx])
+        print(rmse.mean())
